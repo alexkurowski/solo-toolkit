9a1a7232d650e6b68067222eec8f9dcaf6c77732
Alex <kurowski.dev@gmail.com>
2025-01-18 13:15:47 +0800

    Skip default deck files if already present

--- src/utils/deck.ts
+++ src/utils/deck.ts
@@ -101,14 +101,20 @@ export const exportDeck = async (
 
   try {
     await adapter.mkdir(basePath);
+    const existingFiles = await adapter.list(basePath);
     await Promise.all(
-      Object.keys(data).map((key) => {
-        const filename = `${key}.png`;
-        return adapter.writeBinary(
-          basePath + "/" + filename,
-          base64ToArrayBuffer(data[key].trim())
-        );
-      })
+      Object.keys(data)
+        .filter((key) => {
+          const filename = `${key}.png`;
+          return !existingFiles.files?.includes(basePath + "/" + filename);
+        })
+        .map(async (key) => {
+          const filename = `${key}.png`;
+          return adapter.writeBinary(
+            basePath + "/" + filename,
+            base64ToArrayBuffer(data[key].trim())
+          );
+        })
     );
     deckMemo[folderName] = Object.keys(data);
   } catch (error) {
